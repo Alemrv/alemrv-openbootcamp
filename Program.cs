555713f826369f9dc57a98cf3160e004ba15ec16
alemrv <alemrv@gmail.com>
2023-03-22 14:35:25 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace alemrv_openbootcamp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
