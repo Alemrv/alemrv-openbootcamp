e99e1ab66f9b5f937d9a8f859ab27ea6ec2cd1d3
alemrv <alemrv@gmail.com>
2023-03-22 15:20:36 -0400

    Se cambia el nombre de la clase para que coincida con el nombre del ejericio. Se agregan las soluciones a ambos ejercicios del tema 3.

---
+++ intro_EC3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace alemrv_openbootcamp
+{
+    internal class intro_EC3
+    {
+        static void Main(string[] args)
+        {
+            //Resultado Ejercicio 1 Clase 3
+            Console.WriteLine(suma1(1, 2, 3));
+
+            //Resultado Ejercicio 2 Clase 3
+            Coche miCoche = new Coche();
+            miCoche.incrementar();
+            Console.WriteLine(miCoche.puertas);
+        }
+
+        private static int suma1(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    } 
+    public class Coche
+    {
+        public int puertas = 4;
+
+        public void incrementar()
+        {
+            this.puertas++;
+        }
+    }    
+}
